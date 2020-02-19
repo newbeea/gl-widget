a486780404492ed51f22cbe727c135670e230b12
Phil <xupengfei2010@gmail.com>
2020-02-19 19:40:39 +0800

    comment

--- src/Shader.ts
+++ src/Shader.ts
@@ -4,10 +4,10 @@ class Shader {
     gl.shaderSource(shader, glslString)
     gl.compileShader(shader)
     if (gl.getShaderParameter(shader, gl.COMPILE_STATUS) === false) {
-      console.error('THREE.WebGLShader: Shader couldn\'t compile.')
+      console.error('Shader couldn\'t compile.')
     }
     if (gl.getShaderInfoLog(shader) !== '') {
-      console.warn('THREE.WebGLShader: gl.getShaderInfoLog()', type === gl.VERTEX_SHADER ? 'vertex' : 'fragment', gl.getShaderInfoLog(shader))
+      console.warn('gl.getShaderInfoLog()', type === gl.VERTEX_SHADER ? 'vertex' : 'fragment', gl.getShaderInfoLog(shader))
       gl.deleteShader(shader);
     }
     return shader
