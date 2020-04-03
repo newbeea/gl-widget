bd04f269ec549971679cd92e1e97c657959dd17a
Phil <xupengfei2010@gmail.com>
2020-04-03 23:20:10 +0800

    npm script

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,23 @@ import typescript from 'rollup-plugin-typescript2';
 import {terser} from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 
+let isProd = process.env.NODE_ENV === 'production'
+
+const basePlugins = [
+  typescript({ module: 'ESNext' }),
+  babel(),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs({ extensions: ['.js', '.ts'] }),
+]
+const devPlugins = []
+const prodPlugins = [terser()]
+
+let plugins = [...basePlugins].concat(isProd ? prodPlugins : devPlugins)
+
 
 export default {
   input: 'src/index.ts',
@@ -25,16 +42,5 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [
-    typescript({ module: 'ESNext' }),
-    babel(),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
-    commonjs({ extensions: ['.js', '.ts'] }),
-    terser()
-    
-  ]
+  plugins: plugins
 }
