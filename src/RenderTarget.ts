2cdc68339f3e304692451cef8ed8a2e607f3d1b0
phil.xu <010@gmail.com>
2020-04-13 20:03:43 +0800

    depth

--- src/RenderTarget.ts
+++ src/RenderTarget.ts
@@ -7,24 +7,32 @@ class RenderTarget  {
   texture: Texture
   frameBuffer: any
   textureBuffer: any
+  depthTexture: Texture
   gl: WebGLRenderingContext
   textureManager: TextureManager
+  depthBuffer: boolean = true
   constructor (gl: WebGLRenderingContext, width, height, options = {}) {
     this.gl = gl
     this.textureManager = new TextureManager(gl)
     this.width = width
     this.height = height
     this.texture = new Texture()
+    this.depthTexture = new Texture()
+    
+    this.setup(width, height)
+  }
+  setup(width, height) {
+    this.frameBuffer = this.gl.createFramebuffer()
+  
+    this.gl.bindFramebuffer( this.gl.FRAMEBUFFER, this.frameBuffer );
     
     this.textureManager.createTexture(this.texture, width, height)
-
-    this.frameBuffer = gl.createFramebuffer()
-  
-    gl.bindFramebuffer( gl.FRAMEBUFFER, this.frameBuffer );
-    gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.texture.glTexture, null );
-    gl.bindFramebuffer( gl.FRAMEBUFFER, null );
-  }
-  setupDepthTexture (framebuffer, renderTarget) {
+    this.gl.framebufferTexture2D( this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, this.texture.glTexture, 0 );
+    if ( this.depthBuffer ) {
+      this.textureManager.createTexture(this.depthTexture, width, height, this.gl.DEPTH_COMPONENT, this.gl.UNSIGNED_SHORT)
+      this.gl.framebufferTexture2D( this.gl.FRAMEBUFFER, this.gl.DEPTH_ATTACHMENT, this.gl.TEXTURE_2D, this.depthTexture.glTexture, 0 );
+    }
+    this.gl.bindFramebuffer( this.gl.FRAMEBUFFER, null );
   }
   clone () {
     let renderTarget = new RenderTarget(this.gl, this.width, this.height)
--- src/TextureManager.ts
+++ src/TextureManager.ts
@@ -35,15 +35,20 @@ class TextureManager {
     };
     return filterToGL[filter]
   }
-  createTexture(texture: Texture, width, height) {
+
+  
+  createTexture(texture: Texture, width, height, format?, type?) {
     let gl = this.gl
+    format = format || gl.RGBA
+    type = type || gl.UNSIGNED_BYTE
     texture.glTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture.glTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texImage2D(gl.TEXTURE_2D, 0, format, width, height, 0, format, type, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.bindTexture(gl.TEXTURE_2D, null);
+    
     
   }
   setTexture2D(texture: Texture, unit) {
